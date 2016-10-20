80766c09ed684fe158f8b857c6b2733f3bf22888
plwslw <s.lit.winkler@gmail.com>
2016-10-20 15:10:01 -0400

    updating

--- playlist.h
+++ playlist.h
@@ -1,25 +1,10 @@
-#include "myTunez.h"
+#ifndef PLAYLIST
+#define PLAYLIST
+#include "mytunez.h"
 
 
-song_list** playlist(song_node** ptrs){ 
-  int i=0;
-  while(i<26){
-    ptrs[i]=NULL;
-    i++;
-  }
-}
+song_list** playlist(song_node** ptrs);
+void add_song(song_node** playlist, char* name, char* artist);
+song_node* search_song(song_node** playlist, char* name);
 
-void add_song(song_node** playlist, char* name, char* artist){
-  song_node* start;
-  while(playlist) continue;
-  if (start==playlist) playlist=insert_front(playlist, name, artist);
-  else playlist=insert_order(playlist, name, artist);
-}
-  
-song_node* search_song(song_node** playlist, char* name){
-  while(playlist){
-    if (playlist->name==name) return playlist;
-  }
-  return NULL;
-}
-
+#endif
