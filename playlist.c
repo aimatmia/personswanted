3446d3adcc4cbc30f555b6942273d4d1e202dd3c
Mia <mobrien@stuy.edu>
2016-10-21 15:17:41 -0400

    Update playlist.c

--- playlist.c
+++ playlist.c
@@ -1,58 +1,50 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "playlist.h"
-
-
-table* library(){
-  song_node *table[26];
-  int i;
-  for (i=0;i<26;i++){
-    table[i]=NULL;
-  }
-  return table;
-}
-
+	
 //----------------+---------------------
-void add_song(song_node** library, char* name, char* artist){
+void add_song(char* name, char* artist){
   char letter=*artist;
   int pos=letter-'A';
-  insert_order(library[pos], name, artist);
+  insert_order(table[pos], name, artist);
 }
- 
- 
+	 
+	 
 //---------------SEARCH----------------- 
-song_node* search_song(char name[]){
+song_node* find_song(char name[]){
     int i;
-    for (i=0;i<26<i++){
-	    if (!search_song(table[i], name) return search_song(table[i], name);
-    }
+    for (i=0;i<26;i++)
+        if (search_song(table[i], name)) 
+            return search_song(table[i], name);
+        else
+            continue;
     return NULL;
 }
- 		
-song_node* search_artist(char artist[]){
+	 		
+song_node* find_artist(char artist[]){
     int i;
-    for (i=0;i<26<i++){
-        if(!find_artist(table[i], artist)) return (find_artist(table[i], artist));
+    for (i=0;i<26;i++){
+        if(search_artist(table[i], artist)) return (song_node*)search_artist(table[i], artist);
     }
   return NULL;
 }
-
-
+	
+	
 //---------------PRINT--------------
 void print_letter(char letter){
-    print_list(table[letter-'A']);
+    print_all(table[letter-'A']);
     printf("\n");
 }
-
+　
 void print_artist(char artist[]){
     printf("songs for %s-- \n", artist);
-    song_node* singer = find_artist(artist);
+    song_node* singer = (song_node*) find_artist(artist);
     while(singer && strcmp(artist, singer->artist)){
         if (strcmp(singer->artist, artist)) print_song(singer);
         singer=singer->next;
     }
 }
-
+　
 void print_library(){
     //Mia & Stiven's playlist~ :)
     int i;
@@ -60,7 +52,7 @@ void print_library(){
         print_all(table[i]);
     }
 }
-
+　
 void shuffle(){
     printf("shuffle MS playlist up! \n");
     song_node* copy_node = NULL;
@@ -74,23 +66,27 @@ void shuffle(){
         }
     }
     while (copy_node) {
-        song_node* rand_node = random_song(copy_node));
+        song_node* rand_node = random_song(copy_node);
         print_song(rand_node);
-        remove_song(rand_node);
+        copy_node=remove_song(copy_node, rand_node->name, rand_node->artist );
     }
 }
-
-
+　
+　
 //-------------DELETION-------------
 void delete_song(char name[], char artist[]){
-    printf("\n good-bye: *%s - %s*", artist, name);
-    table[artist[0]-'A'] = remove_song(table[artist[0]-'A'], name, artist);
+    printf("\n attemping to delete-song: *%s - %s*", artist, name);
+    int bkt = artist[0]-'A';
+    table[bkt] = remove_song(table[bkt], name, artist);
     print_library();
 }
-
+　
 void delete_lib(){
     int i=0;
     for (i=0;i<26;i++){
-        table[i] = free_all(table[i]);
+        free_all(table[i]);
     }
 }
+　
+　
+　
