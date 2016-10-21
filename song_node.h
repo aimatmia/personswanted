8e70f871498802ab771f2152f3cd37a43b2d9b57
Mia <mobrien@stuy.edu>
2016-10-21 15:20:37 -0400

    Update song_node.h

--- song_node.h
+++ song_node.h
@@ -10,15 +10,12 @@ song_node* insert_order(song_node* next, char name[], char artist[]);
 void print_all(song_node* song);
 void print_song(song_node* song);
 
-song_node* find_song(song_node* rt, char name[]);
-song_node* find_artist(song_node* rt, char artist[]);
+//song_node* find_song(song_node* rt, char name[]);
+song_node* search_artist(song_node* rt, char artist[]);
 song_node* search_song(song_node* song, char name[]);
 
 int num_song(song_node* rt);
 song_node* random_song(song_node* rt);
 
 song_node* remove_song(song_node* rt, char name[], char artist[]);
-void free_all(song_node* rt);
-
-
-  
+void free_all(song_node* rt); 
