bd1c7f9f8b4237618ae570b9fd30d67f9a0c0e1d
Mia <mobrien@stuy.edu>
2016-10-21 04:31:58 -0400

    Update song_node.h

--- song_node.h
+++ song_node.h
@@ -5,15 +5,20 @@ typedef struct song_node{
 } song_node;
 
 song_node* insert_front(song_node* rt, char name[], char artist[]);
-song_node* insert_order(song_node* first, char name[], char artist[]);
+song_node* insert_order(song_node* next, char name[], char artist[]);
 
-void print_song(song_node* song);
 void print_all(song_node* song);
+void print_song(song_node* song);
 
 song_node* find_song(song_node* rt, char name[]);
 song_node* find_artist(song_node* rt, char artist[]);
+song_node* search_song(song_node* song, char name[]);
 
+int num_song(song_node* rt);
 song_node* random_song(song_node* rt);
-song_node* remove_song(song_node* sacrifice);
-  
+
+song_node* remove_song(song_node* rt, char name[], char artist[]);
+void free_all(song_node* rt);
+
+
   
