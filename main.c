46c73d2ae400b7f3ab2fe3d263e864a648edae93
Mia <mobrien@stuy.edu>
2016-10-21 21:19:30 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,164 +1,105 @@
 #include <stdio.h> #include <stdlib.h>
 #include <string.h>
 #include "song_node.h"
-　
-　
+
+void print_list(song_node* sn) {
+    if(sn==NULL)
+        printf("No song found\n");
+    else
+        print_song(sn);
+ 
+}
+
 int main() {
   //initializes library nodes to null
 	init_lib();
-　
+
 	//add fav beats to playlist
-	char name[40] = "Bring Me To Life";
-	char artist[40] = "Evanescence";
-	add_song( name, artist);
-　
-	strcpy(name, "Surrender");
-	strcpy(artist, "Evanescence");
-	add_song( name, artist);
-　
-	strcpy(name, "Locked Out of Heaven");
-	strcpy(artist, "Bruno Mars");
-	add_song( name, artist);
-　
-	strcpy(name, "Runaway Baby");
-	strcpy(artist, "Bruno Mars");
-	add_song( name, artist);
-　
-	strcpy(name, "Fireflies");
-	strcpy(artist, "Owl City");
-	add_song( name, artist);
-　
-	strcpy(name, "Tokyo");
-	strcpy(artist, "Owl City");
-	add_song( name, artist);
-　
-	strcpy(name, "Emperor's New Clothes");
-	strcpy(artist, "Panic! at the Disco");
-	add_song( name, artist);
+
+	add_song( "Bring Me To Life", "Evanescence");
+	add_song("Surrender", "Evanescence");
+	add_song( "Please, Please, Please Let Me Get What I Want", "The Smiths");
+	add_song( "Locked Out of Heaven", "Bruno Mars");
+	add_song( "Runaway Baby", "Bruno Mars");
+	add_song( "Fireflies", "Owl City");
+	add_song( "Tokyo", "Owl City");
+
+	add_song( "Emperor's New Clothes", "Panic! at the Disco");
   
-  strcpy(name, "Death of a Bachelor");
-	strcpy(artist, "Panic! at the Disco");
-	add_song( name, artist);
+	add_song( "Death of a Bachelor", "Panic! at the Disco");
   
-  strcpy(name, "Counting Stars");
-	strcpy(artist, "One Republic");
-	add_song( name, artist);
-　
-	strcpy(name, "Please, Please, Please Let Me Get What I Want");
-	strcpy(artist, "The Smiths");
-	add_song( name, artist);
-　
-	strcpy(name, "Contagious");
-	strcpy(artist, "Night Riots");
-	add_song( name, artist);
-  
-  strcpy(name, "Red Flag");
-	strcpy(artist, "The Moth & The Flame");
-	add_song( name, artist);
-  
-	strcpy(name, "Crystals");
-	strcpy(artist, "Of Monsters and Men");
-	add_song( name, artist);
-  
-  strcpy(name, "For him.");
-	strcpy(artist, "Troye Sivan");
-	add_song( name, artist);
-  
-	strcpy(name, "Half Life");
-	strcpy(artist, "Duncan Sheikh");
-	add_song( name, artist);
-  
-  strcpy(name, "Good Life");
-	strcpy(artist, "Leslie Mills");
-	add_song( name, artist);
-　
-	strcpy(name, "In The End");
-	strcpy(artist, "LINKIN Park");
-	add_song( name, artist);
-　
-	strcpy(name, "Closer");
-	strcpy(artist, "Tegan and Sara");
-	add_song( name, artist);
-  
-  strcpy(name, "Big Honkin Hole in My Heart");
-	strcpy(artist, "Phineas and Ferb");
-	add_song( name, artist);
-  
-  strcpy(name, "Sk8er Boi");
-	strcpy(artist, "Avril Lavigne");
-	add_song( name, artist);
-　
+	add_song( "Counting Stars", "One Republic");
+	add_song( "Contagious", "Night Riots");
+	add_song( "Red Flag", "The Moth & The Flame");
+	add_song("Crystals","Of Monsters and Men");
+//	add_song( "For him.", "Troye Sivan");
+	add_song( "Half Life", "Duncan Sheikh");
+//	add_song( "Good Life", "Leslie Mills");
+	add_song( "In The End", "LINKIN Park");
+//	add_song( "CLoser", "Tegan and Sara");
+	add_song( "Big Honkin Hole in My Heart", "Phineas and Ferb");
+	add_song( "Sk8er Boi", "Avril Lavigne");
+
 	//print the entire library to make sure everything was added correctly 
 	printf("\n\n Mia & Steven's Playlist~ \n");
 	print_library();
-  printf("\n");
-　
+
 	//Search by artist/singer
 	printf("\n Skimming for a song by Panic! at the Disco : \n");
-	strcpy(artist, "Panic! at the Disco");
-	find_artist(artist);
+	print_list(find_artist( "Panic! at the Disco"));
   
   printf("\n Skimming for a song by Phineas and Ferb : \n");
-	strcpy(artist, "Phineas and Ferb");
-	find_artist( artist);
-　
+	print_list(find_artist( "Phineas and Ferb"));
+
 	printf("\n Skimming for a song by No Doubt : \n");
-	strcpy(artist, "No Doubt");
-	find_artist( artist);
-　
-　
+	print_list(find_artist( "No Doubt"));
+
 　
 	//Print by letter
-	printf("\n Printing songs that begin with the letter 'C': \n");
-	char letter = 'C';
+	printf("\n Printing songs that begin with the letter 'P': \n");
+	char letter = 'P';
 	print_letter( letter);
-　
+
 　
 	//Printing by artist
 	printf("\n Printing all songs by Evanescence: \n");
-	strcpy(artist, "Evanescence");
-	print_artist( artist);
-　
-　
+	print_artist( "Evanescence");
+
 	//Shuffling playlist (with 9 random songs)
 	printf("\n Shuffled playlist of 9 random songs : \n");
-	shuffle( 9);
+	shuffle(9);
   
-　
+
 	//Deleting songs from the library:
 	printf("\n === Testing delete_song === : \n");
 	printf("--------------------------------------------------------------------- \n");
 	printf("Original Playlist: \n");
 	print_library();
   
-　
+
 	printf("\n good-bye to: 'In The End' \n");
-	strcpy(name, "In The End");
-	delete_song( name);
+	delete_song( "In The End", "LINKIN Park");
 	print_library();
-　
+
 　
 	printf("\n good-bye to: 'Please, Please, Please Let Me Get What I Want' \n");
-	strcpy(name, "Please, Please, Please Let Me Get What I Want");
-	delete_song( name);
+	delete_song( "Please, Please, Please Let Me Get What I Want", "The Smiths");
 	print_library();
-　
+
 　
 　
 	printf("\n === Setting the Playlist Free === \n");
 	printf("------------------------------------------------- \n");
-　
+
 	printf("Original M&S Playlist: \n");
 	print_library();
-　
+
 　
 	printf("\n Freed All the Songs \n");
 	delete_lib();
-	print_library(); //printing an empty playlist
   
 	printf("\n Playlist empty, no songs left :( \n");
-  printf("How 'bout you add some more songs?~"); 
-　
+        printf("How 'bout you add some more songs?~\n"); 
+
         return 0;
 }
-　
